7af1e767b095dad27eb8e30ae3e61ba007ceb856
Olga <olga_hom@mail.ru>
2023-03-01 21:31:20 +0300

    hw5 task3

--- HomeWorkJava/HomeWork5/task2.java
+++ HomeWorkJava/HomeWork5/task2.java
@@ -1,31 +1,57 @@
-
-
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-//Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
+
+// Пусть дан список сотрудников. Написать программу, которая найдет и выведет повторяющиеся имена 
+// с количеством повторений. Отсортировать по убыванию популярности Имени.
 public class task2 {
     public static void main(String[] args) {
-        Map<String, Integer> listWorkers = new HashMap<>();
-        String workers = "Иван Иванов Светлана Петрова Кристина Белова Анна Мусина Анна Крутова Иван Юрин Петр Лыков Павел Чернов Петр Чернышов Мария Федорова Марина Светлова Мария Савина Мария Рыкова Марина Лугова Анна Владимирова Иван Мечников Петр Петин Иван Ежов ";
-
-        String[] NamesLastNames = workers.split(" ");
-        
-        for (int i = 0; i < NamesLastNames.length; i += 2) {
-            if (listWorkers.containsKey(NamesLastNames[i])) {
-                listWorkers.replace(NamesLastNames[i], listWorkers.get(NamesLastNames[i]) + 1);
-                
+        Map<String, Integer> nameMap = new HashMap<>();
+        String workers = "Иван Иванов " +
+                "Светлана Петрова " +
+                "Кристина Белова " +
+                "Анна Мусина " +
+                "Анна Крутова " +
+                "Иван Юрин " +
+                "Петр Лыков " +
+                "Павел Чернов " +
+                "Петр Чернышов " +
+                "Мария Федорова " +
+                "Марина Светлова " +
+                "Мария Савина " +
+                "Мария Рыкова " +
+                "Марина Лугова " +
+                "Анна Владимирова " +
+                "Иван Мечников " +
+                "Петр Петин " +
+                "Иван Ежов ";
+        String[] listNamesLastname = workers.split(" ");
+        for (int i = 0; i < listNamesLastname.length; i += 2) {
+            if (nameMap.containsKey(listNamesLastname[i])) {
+                nameMap.replace(listNamesLastname[i], nameMap.get(listNamesLastname[i]) + 1);
             } else {
-                listWorkers.put(NamesLastNames[i], 1);
+                nameMap.put(listNamesLastname[i], 1);
             }
         }
-        System.out.println("\nСписок имен:");
-      
-        System.out.println(listWorkers);
-
-        System.out.println("\nСписок имен по обыванию популярности:");
-        listWorkers.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .forEach(System.out::println);
+        System.out.println("Спиок имен с количеством повторений: " +nameMap);
+        Map<String, Integer> sortedNameMap = new LinkedHashMap<>();
+        int max = 1;
+        for (int value : nameMap.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        for (int i = max; i > 0; i--) {
+            for (Map.Entry<String, Integer> entry : nameMap.entrySet()) {
+                String key = entry.getKey();
+                if (nameMap.get(key) == i) {
+                    sortedNameMap.put(key, nameMap.get(key));
+                }
+            }
+        }
+        System.out.println("Повторений имен с количеством, отсортированное по убыванию: " );
+        sortedNameMap.forEach((key, value) -> System.out.println(key + " : " + value));
+        
     }
-
 }
